06b20982029cfe5e1bb33357be47409624267201
Besart <besartzeka@hotmail.com>
2021-10-01 14:12:02 +0200

    First commit

---
+++ app/Models/Employee.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Country;
+use App\Models\Department;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'middle_name',
+        'address',
+        'department_id',
+        'country_id',
+        'state_id',
+        'city_id',
+        'zip_code',
+        'birthdate',
+        'date_hired'
+    ];
+
+    /**
+     * Get the department associated with the Employee
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function department()
+    {
+        return $this->hasOne(Department::class, 'id', 'department_id');
+    }
+
+    
+}
---
+++ app/Models/State.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Country;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class State extends Model
+{
+    use HasFactory;
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function cities()
+    {
+        return $this->hasMany(City::class);
+    }
+
+    public $fillable = ['country_id', 'name'];
+}
